725b493bf4d2745deda3479b749f1b447863dae6
CrystalUbadiniru <78229144+CrystalUbadiniru@users.noreply.github.com>
2021-04-11 18:04:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname,"frontend","static")));
+app.use("/static", express.static(path.resolve(__dirname,"index.js")));
 
 app.get("/*", (req,res) =>{
       res.sendFile(path.resolve(__dirname,"index.html"))
