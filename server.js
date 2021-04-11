2e1b20e1b8356b0015a167af3729039273f6ede1
CrystalUbadiniru <78229144+CrystalUbadiniru@users.noreply.github.com>
2021-04-11 18:03:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use("/static", express.static(path.resolve(__dirname,"frontend","static")));
 
 app.get("/*", (req,res) =>{
-      res.sendFile(path.resolve(__dirname,"frontend", "index.html"))
+      res.sendFile(path.resolve(__dirname,"index.html"))
 })
 
 app.listen(process.env.PORT || 5500 , () => console.log("Running on port 5500"))
