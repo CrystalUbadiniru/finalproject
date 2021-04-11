9fe6f24e585cb9d4af3772552d647a44742a4c22
CrystalUbadiniru <78229144+CrystalUbadiniru@users.noreply.github.com>
2021-04-11 18:34:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname,"index.js")));
+app.use("/static", express.static(path.resolve(__dirname,"finalproject", "index.js")));
 app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap'));
 
 
 app.get("/*", (req,res) =>{
-      res.sendFile(path.resolve(__dirname,"index.html"))
+      res.sendFile(path.resolve(__dirname,"finalproject", "index.html"))
 })
 
 app.listen(process.env.PORT || 5500 , () => console.log("Running on port 5500"))
